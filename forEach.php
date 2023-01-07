6a5a4f57c738127bcebce283eefd11e38e4eb07d
Alberto Rodríguez Taboada <albertrodtab@gmail.com>
2023-01-07 19:03:00 +0100

    Subo estructura inicial del proyecto

---
+++ forEach.php
@@ -0,0 +1,24 @@
+<?php
+include "header.php";
+?>
+
+<section>
+
+    <div class="body">
+
+        <h1>Estructura de control For Each</h1>
+
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae commodi delectus dicta distinctio ea
+                eaque, eveniet excepturi facere, hic iusto pariatur quibusdam reprehenderit repudiandae unde ut vero
+                voluptate, voluptates!</p>
+        </div>
+
+
+    </div>
+
+
+
+</section>
+
+<?php include "footer.php.php";
